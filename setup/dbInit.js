d774a0e096bdea139263036dd9bdf9dcf3aad079
Alexander Volkov <sander.chief@gmail.com>
2025-07-26 21:34:40 +0200

    Refactored dbInit to remove repeating db logic

--- setup/dbInit.js
+++ setup/dbInit.js
@@ -1,31 +1,39 @@
 import sqlite3 from 'sqlite3';
+import path from 'path';
 
-try {
-  const db = new sqlite3.Database('./todo.db');
+const dbPath = 'todo.db';
 
-  db.serialize(() => {
-    db.run('PRAGMA foreign_keys = ON');
+export const dbInit = (dbPath) => {
+  try {
+    const dbFile = path.resolve(path.dirname('./'), dbPath);
+    const db = new sqlite3.Database(dbFile);
+  
+    db.serialize(() => {
+      db.run('PRAGMA foreign_keys = ON');
+  
+      db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL
+        )
+      `);
+  
+      db.run(`
+        CREATE TABLE IF NOT EXISTS todos (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          done BOOLEAN,
+          content TEXT,
+          user_id INTEGER,
+          FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        )
+      `);
+    });
+  
+    db.close();
+  } catch (error) {
+    console.log(error);
+  }  
+};
 
-    db.run(`
-      CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-      )
-    `);
-
-    db.run(`
-      CREATE TABLE IF NOT EXISTS todos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        done BOOLEAN,
-        content TEXT,
-        user_id INTEGER,
-        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-      )
-    `);
-  });
-
-  db.close();
-} catch (error) {
-  console.log(error);
-}
+dbInit(dbPath);
--- setup/playwright.setup.ts
+++ setup/playwright.setup.ts
@@ -1,35 +1,7 @@
-import sqlite3 from 'sqlite3';
-import path from 'path';
+import { dbInit } from './dbInit';
 
-export default async function globalSetup() {
-  try {
-    const dbFile = path.resolve(path.dirname('./'), 'todo-e2e.db');
-    const db = new sqlite3.Database(dbFile);
+export default function globalSetup() {
+  const dbPath = 'todo-e2e.db';
 
-    db.serialize(() => {
-      db.run('PRAGMA foreign_keys = ON');
-  
-      db.run(`
-        CREATE TABLE IF NOT EXISTS users (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          username TEXT UNIQUE NOT NULL,
-          password TEXT NOT NULL
-        )
-      `);
-  
-      db.run(`
-        CREATE TABLE IF NOT EXISTS todos (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          done BOOLEAN,
-          content TEXT,
-          user_id INTEGER,
-          FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-        )
-      `);
-    });
-
-    db.close();
-  } catch (error) {
-    console.log(error);
-  }
+  dbInit(dbPath);
 }
